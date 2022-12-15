3ac8c1532c10dea289fc9498a6dc0295508945fc
m4nk9i <m4nk9i@gmail.com>
2022-12-15 06:25:42 +0100

    initial commit - blank window based on this tutorial https://www.youtube.com/watch?v=KcEvHq8Pqs0

---
+++ MyFrame.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+    MyFrame()
+    {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+    }
+    
+}
